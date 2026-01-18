35732299c94d03f1d33b30444863bd6fa817dd3c
Pvithor <58409481+Pvithor@users.noreply.github.com>
2026-01-17 22:12:33 -0400

    WEBBOOK

---
+++ webwood.py
@@ -0,0 +1,25 @@
+import requests 
+import os
+from time import sleep
+import streamlit as st
+
+# webhook_token = os.getenv("TOKEN_WEBHOOK")
+url = f"https://discord.com/api/webhooks/{st.secrets['TOKEN_WEBHOOK']}"
+
+def main(msg):
+    global url
+    data = {
+    "content" : f"{msg}",
+    "username" : "LOG_STREMILIT"
+    }
+    result = requests.post(url, json = data)
+    sleep(0.5)    
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print('erro')
+        sleep(1)
+    else: pass
+
+if __name__ == "__main__":
+    main("teste")
